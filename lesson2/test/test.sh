1afc0587718c86b301d89fb13e496c168a5b1a36
olya <returnolya@gmail.com>
2020-02-23 15:14:49 +0200

    initial on PC

---
+++ lesson2/test/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Something ..."
+whoami
+pwd
+
+echo "it's my derictory$HOME"
+
+index=5
+name="Olya"
+echo "$name has index = $index"
+
+var1=$(( 10+30*4 ))
+var2=$(( $var1 / 2))
+echo $var1
+echo $var2
+
+if pwd
+then
+echo "works"
+fi
+
